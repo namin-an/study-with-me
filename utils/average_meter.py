dd76f16bc042e904c5d70e74f503dde8ededfa75
namin-an <namin0202@gmail.com>
2022-06-21 20:22:58 +0900

    feat: add max value

--- utils/average_meter.py
+++ utils/average_meter.py
@@ -7,11 +7,17 @@ class AverageMeter(object):
         self.avg = 0
         self.sum = 0
         self.count = 0
+        self.max_val = None
     
     def update(self, val, n=1):
         self.val = val
         self.sum += val*n
         self.count += n
         self.avg = self.sum / self.count
+        if self.count == 1:
+            self.max_val = val
+        else:
+            if val > self.max_val:
+                self.max_val = val
         
   
