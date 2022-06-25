a7d1ab7be547cdb06df8de54e7aaef32cbba0efa
namin-an <namin0202@gmail.com>
2022-06-25 20:27:09 +0900

    feat: change reward

--- FUNCTIONS/common/Environment.py
+++ FUNCTIONS/common/Environment.py
@@ -1,14 +1,20 @@
 # Reference: https://github.com/namin-an/pytorch-DRL/blob/master/common/Model.py
 
+import os
 import torch
 import torch.nn as nn
 
+
+
 class Env:
-    def __init__(self, device, net, criterion, checkpoint_file_agents, WINDOW_SIZE, SUBJECT_NUM, FOLD_NUM):
+    def __init__(self, device, net, optimizer, last_trans, criterion, checkpoint_file_agents, mask_path, WINDOW_SIZE, SUBJECT_NUM, FOLD_NUM):
         self.device = device
         self.net = net
+        self.optimizer = optimizer
+        self.last_trans = last_trans
         self.criterion = criterion
         self.checkpoint_file_agents = checkpoint_file_agents
+        self.mask_path = mask_path
         self.WINDOW_SIZE = WINDOW_SIZE
         self.SUBJECT_NUM = SUBJECT_NUM
         self.FOLD_NUM = FOLD_NUM
@@ -20,31 +26,36 @@ class Env:
         states = features[:, t:t+self.WINDOW_SIZE] * mask[:, t:t+self.WINDOW_SIZE]
         return states
     
-    def step(self, episode, t, actions, states, features, targets, BATCH_SIZE, mask, max_reward):
-
-        # original loss on the pretrained model
-        loss_FM = self.criterion(self.net.classification(features), targets.squeeze())
+    def step(self, episode, t, actions, states, inputs, targets, mask, max_reward):
         
         mask[:, t] = actions
+        features = self.net.extraction(inputs) 
         assert features.shape == mask.shape
-        new_features = features * mask
+        new_features = features * mask 
         
         if (t >= 0 and t <= (features.shape[-1]-self.WINDOW_SIZE)): 
-            loss_AM = self.criterion(self.net.classification(new_features), targets.squeeze())
+            # Loss_FM -> ERROR
+            outputs = self.net.classification(new_features)    
+            loss_AM = self.criterion(outputs, targets.squeeze())  
+            outputs = self.last_trans(outputs)
+            probs, preds = torch.max(outputs.detach(), dim=1)
+            acc = (preds == targets.squeeze()).sum() / float(preds.shape[0])
             
-            reward = loss_FM - loss_AM
-            rewards = torch.tile(reward, (BATCH_SIZE, 1))
-            if (t > 0 and max_reward.item() <= reward.item()):
-                torch.save(mask, f'/opt/pytorch/demo/masks/final_mask_subject{self.SUBJECT_NUM}_fold{self.FOLD_NUM}_{episode}_{t}.pt')
-
-            if (t == features.shape[-1]-self.WINDOW_SIZE) or bool(reward.item() > 1.0 and loss_AM.item() < 1.0):
+            reward = acc
+            rewards = torch.tile(reward, (features.shape[0], 1))
+            
+            self.optimizer.zero_grad()
+            loss_AM.backward()
+            self.optimizer.step()  
+            
+            if (t > 0 and max_reward.item() <= reward.item()):              
+                torch.save(self.net.state_dict(), self.checkpoint_file_agents)
+                
+            if (t == features.shape[-1]-self.WINDOW_SIZE):
                 next_states, rewards, done, loss_AM = None, None, True, None
             else:
-                done = False
-            
+                done = False            
                 next_states = self.reset(t+1, features, mask)  
                 next_states = next_states - states
-        else:
-            next_states, rewards, done, loss_AM = None, None, True, None
             
         return next_states, rewards, done, loss_AM, mask
